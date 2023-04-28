55f9a8d7f6640d8c6f5a31c6044e1af7906fafad
Yoruyeh <orchardsky31@gmail.com>
2023-04-28 20:57:43 +0800

    add step1.js and add to register.js

--- src/components/register.js
+++ src/components/register.js
@@ -1,11 +1,14 @@
 import StepProgress from "./step_progress"
-
+import Step1 from "./step1";
 
 function Register() {
   return (
-    <section class="register-container col col-lg-6 col-sm-12" data-phase="1" data-total-price="0">
+  <section class="register-container col col-lg-6 col-sm-12" data-phase="1" data-total-price="0">
       <StepProgress />
+    <section class="form-container col col-12">
+      <Step1 />
     </section>
+  </section>
   )
 }
 
