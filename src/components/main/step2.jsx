ae0706db4916fec4a3d6d6663f4056433c11386e
Yoruyeh <orchardsky31@gmail.com>
2023-05-04 21:05:27 +0800

    add the key prop

--- src/components/main/step2.jsx
+++ src/components/main/step2.jsx
@@ -1,7 +1,7 @@
 
-function FormContent({ text, price, time }) {
+function Form2({ text, price, time }) {
   return (
-    <label class="radio-group col col-12" data-price={price}>
+    <label class="radio-group col col-12" data-price={price} key={price}>
       <input id="shipping-standard" type="radio" name="shipping"  checked/>
       <div class="radio-info">
         <div class="col col-12">
@@ -24,8 +24,8 @@ export default function  Step2() {
     <form class="col col-12" data-phase="shipping">
       <h3 class="form-title">運送方式</h3>
       <section class="form-body col col-12">
-        <FormContent text={'標準運送'} price={0} time={'約 3~7 個工作天'}/>
-        <FormContent text={'DHL 貨運'} price={500} time={'48 小時內送達'}/>
+        <Form2 text={'標準運送'} price={0} time={'約 3~7 個工作天'}/>
+        <Form2 text={'DHL 貨運'} price={500} time={'48 小時內送達'}/>
       </section>
     </form>
   )
