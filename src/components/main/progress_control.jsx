a7f2ace4db199f4c344d28638dc9f4cdb12357e7
Yoruyeh <orchardsky31@gmail.com>
2023-05-05 15:32:04 +0800

    add keys

--- src/components/main/progress_control.jsx
+++ src/components/main/progress_control.jsx
@@ -1,9 +1,9 @@
 import { ReactComponent as RightArrowIcon} from '../../icons/right-arrow.svg'
 import { ReactComponent as LeftArrowIcon} from '../../icons/left-arrow.svg'
 
-function ButtonGroup( {phase, children} ) {
+function ButtonGroup( {phase, onClick, children} ) {
   return (
-    <section className="button-group col col-12" data-phase={phase}>
+    <section className="button-group col col-12" data-phase={phase} key={phase} onClick={onClick}>
       {children}
     </section>
   )
@@ -12,14 +12,14 @@ function ButtonGroup( {phase, children} ) {
 function Buttons( {text, className} ) {
     if (className === 'prev') {
       return (
-      <button className={className}>
+      <button className={className} key={className}>
         <LeftArrowIcon className="cursor-point" />
         {text}
       </button>
       )
     } else {
       return (
-      <button className={className}>
+      <button className={className} key={className}>
         {text}
         <RightArrowIcon className="cursor-point" />
       </button>
@@ -32,15 +32,15 @@ export default function ProgressControl() {
     <>
       <section className="progress-control-container col col-lg-6 col-sm-12">
         <ButtonGroup phase={"address"}>
-          <Buttons className="next" text={"下一步"}/>
+          <Buttons className="next" text={"下一步"} />
         </ButtonGroup>
         <ButtonGroup phase={"shipping"}>
-          <Buttons className="prev" text={"上一步"}/>
-          <Buttons className="next" text={"下一步"}/>
+          <Buttons className="prev" text={"上一步"} />
+          <Buttons className="next" text={"下一步"} />
         </ButtonGroup>
         <ButtonGroup phase={"credit-card"}>
-          <Buttons className="prev" text={"上一步"}/>
-          <Buttons className="next" text={"確認下單"}/>
+          <Buttons className="prev" text={"上一步"} />
+          <Buttons className="next" text={"確認下單"} />
         </ButtonGroup>
       </section>
     </>
